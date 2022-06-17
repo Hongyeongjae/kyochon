baafaa4079baf04ac48a128b4ead0735124aeaf9
Hongyeongjae <hyj155@naver.com>
2022-06-17 17:27:15 +0900

    마무으리

---
+++ java/ex1/main/java/src/Car.java
@@ -0,0 +1,8 @@
+package src;
+public class Car {
+   public String company = "Genesis";
+   public String model = "G90";
+   public String color = "black";
+   public int maxSpeed = 400;
+   public int speed = 200; 
+}
