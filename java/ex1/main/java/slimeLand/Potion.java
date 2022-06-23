d222078d939eaa73c051187d12a299b8eaa294da
Hongyeongjae <hyj155@naver.com>
2022-06-23 17:23:28 +0900

    화이팅..

---
+++ java/ex1/main/java/slimeLand/Potion.java
@@ -0,0 +1,16 @@
+package slimeLand;
+
+import java.util.ArrayList;
+
+public class Potion {
+    
+    
+    ArrayList<Potion> itemList = new ArrayList<Potion>();
+        itemList.add(new Item())
+
+    public list(String n, int num, int g) {
+        ItemName = n;
+        Itemumber = num;
+        ItemEffect = g;
+    }
+}
